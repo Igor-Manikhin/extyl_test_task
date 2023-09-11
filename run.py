671a75d39f1c6d0decee978e64bb95ebe6bf9a23
Igor <manikhin@holyweb.ru>
2023-09-11 22:00:30 +0300

    Updated data processing implementation

--- run.py
+++ run.py
@@ -9,10 +9,11 @@ from core.utils import generate_json_list
 
 async def main():
     await init()
-    json_list = generate_json_list(list_size=settings.LIST_DATA_COUNT)
 
     while True:
         print("Start processing json data list")
+
+        json_list = generate_json_list(list_size=settings.LIST_DATA_COUNT)
         await process_data(json_list)
         print(f"Processing json data list is completed. Sleep on {SLEEP_TIME_SECONDS} seconds")
         await asyncio.sleep(SLEEP_TIME_SECONDS)
